d6debd45c4d54dec8f17dcad5115b34957c46935
t.ismoilov <t.ismoilov@paymo.uz>
2023-04-30 18:03:11 +0500

    first commit

---
+++ src/main/java/me/timur/findguideback/FindGuideBackApplication.java
@@ -0,0 +1,13 @@
+package me.timur.findguideback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindGuideBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindGuideBackApplication.class, args);
+    }
+
+}
