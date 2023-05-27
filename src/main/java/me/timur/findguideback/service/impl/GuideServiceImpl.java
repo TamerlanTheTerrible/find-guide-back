cd27e1bad7c1bb7eb947821334b73c22b95c9a84
t.ismoilov <t.ismoilov@paymo.uz>
2023-05-27 13:25:20 +0500

    guide update

--- src/main/java/me/timur/findguideback/service/impl/GuideServiceImpl.java
+++ src/main/java/me/timur/findguideback/service/impl/GuideServiceImpl.java
@@ -2,10 +2,9 @@ package me.timur.findguideback.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.timur.findguideback.entity.File;
-import me.timur.findguideback.entity.Guide;
-import me.timur.findguideback.entity.Transport;
+import me.timur.findguideback.entity.*;
 import me.timur.findguideback.exception.FindGuideException;
+import me.timur.findguideback.model.dto.FileCreateDto;
 import me.timur.findguideback.model.dto.GuideCreateOrUpdateDto;
 import me.timur.findguideback.model.dto.GuideDto;
 import me.timur.findguideback.model.enums.ResponseCode;
@@ -13,6 +12,7 @@ import me.timur.findguideback.repository.*;
 import me.timur.findguideback.service.GuideService;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,62 +29,37 @@ public class GuideServiceImpl implements GuideService {
     private final LanguageRepository languageRepository;
     private final RegionRepository regionRepository;
     private final TransportRepository transportRepository;
-    private final FileRepository fileRepository;
-
     @Override
     public GuideDto save(GuideCreateOrUpdateDto requestDto) {
-        var languages = languageRepository.findAllByEngNameIn(requestDto.getLanguageNames());
-        if (languages.isEmpty()) {
-            throw new FindGuideException(ResponseCode.NOT_FOUND, "Could not find languages with names: " + requestDto.getLanguageNames());
-        }
-
-        var regions = regionRepository.findAllByEngNameIn(requestDto.getRegionNames());
-        if (regions.isEmpty()) {
-            throw new FindGuideException(ResponseCode.NOT_FOUND, "Could not find regions with names: " + requestDto.getRegionNames());
-        }
-
-        Set<Transport> transports = null;
-        if (requestDto.getTransports() != null && !requestDto.getTransports().isEmpty()) {
-            transports = transportRepository.findAllByEngNameIn(requestDto.getTransports());
-            if (transports.isEmpty()) {
-                throw new FindGuideException(ResponseCode.NOT_FOUND, "Could not find transports with names: " + requestDto.getTransports());
-            }
-        }
-
-        Set<File> files = null;
-        if (requestDto.getFiles() != null) {
-            files = requestDto.getFiles().stream().map(File::new).collect(Collectors.toSet());
-        }
-
+        var languages = getLanguages(requestDto.getLanguageNames());
+        var regions = getRegions(requestDto.getRegionNames());
+        var transports = getTransports(requestDto.getTransports());
+        var files = getFiles(requestDto.getFiles());
         var user = userRepository.findByTelegramId(requestDto.getUserTelegramId()).orElseThrow(() -> new FindGuideException(ResponseCode.NOT_FOUND, "Could not find user with telegram id: " + requestDto.getUserTelegramId()));
         var guide = guideRepository.save(new Guide(requestDto, user, languages, regions, transports, files));
         return new GuideDto(guide);
     }
-
     @Override
     public GuideDto update(GuideCreateOrUpdateDto requestDto) {
         var user = guideRepository.findByUserIdOrUserTelegramId(requestDto.getUserId(), requestDto.getUserTelegramId())
                 .orElseThrow(() -> new FindGuideException(ResponseCode.NOT_FOUND, "Could not find guide with user id: " + requestDto.getUserId() + " or telegram id: " + requestDto.getUserTelegramId()));
         if (requestDto.getLanguageNames() != null && !requestDto.getLanguageNames().isEmpty()) {
-            user.setLanguages(
-                    languageRepository.findAllByEngNameIn(requestDto.getLanguageNames())
-            );
+            user.setLanguages(getLanguages(requestDto.getLanguageNames()));
         }
         if (requestDto.getRegionNames() != null && !requestDto.getRegionNames().isEmpty()) {
-            user.setRegions(
-                    regionRepository.findAllByEngNameIn(requestDto.getRegionNames())
-            );
+            user.setRegions(getRegions(requestDto.getRegionNames()));
         }
         if (requestDto.getFiles() != null && !requestDto.getFiles().isEmpty()) {
-            user.setFiles(
-                    requestDto.getFiles().stream().map(File::new).collect(Collectors.toSet())
-            );
+            user.setFiles(getFiles(requestDto.getFiles()));
         }
         if (requestDto.getDescription() != null && !requestDto.getDescription().isEmpty()) {
             user.setDescription(requestDto.getDescription());
         }
         if (requestDto.getHasCar() != null) {
             user.setHasCar(requestDto.getHasCar());
+        }
+        if (requestDto.getTransports() != null && !requestDto.getTransports().isEmpty()) {
+            user.setTransports(getTransports(requestDto.getTransports()));
         }
 
         return new GuideDto(guideRepository.save(user));
@@ -102,5 +77,40 @@ public class GuideServiceImpl implements GuideService {
         return guideRepository.findById(telegramId)
                 .map(GuideDto::new)
                 .orElseThrow(() -> new FindGuideException(ResponseCode.NOT_FOUND, "Could not find guide with telegram id: " + telegramId));
+    }
+
+    private Set<File> getFiles(Collection<FileCreateDto> args) {
+        Set<File> files = null;
+        if (args != null) {
+            files = args.stream().map(File::new).collect(Collectors.toSet());
+        }
+        return files;
+    }
+
+    private Set<Transport> getTransports(Collection<String> args) {
+        Set<Transport> transports = null;
+        if (args != null && !args.isEmpty()) {
+            transports = transportRepository.findAllByEngNameIn(args);
+            if (transports.isEmpty()) {
+                throw new FindGuideException(ResponseCode.NOT_FOUND, "Could not find transports with names: " + args);
+            }
+        }
+        return transports;
+    }
+
+    private Set<Region> getRegions(Collection<String> args) {
+        var regions = regionRepository.findAllByEngNameIn(args);
+        if (regions.isEmpty()) {
+            throw new FindGuideException(ResponseCode.NOT_FOUND, "Could not find regions with names: " + args);
+        }
+        return regions;
+    }
+
+    private Set<Language> getLanguages(Collection<String> args) {
+        var languages = languageRepository.findAllByEngNameIn(args);
+        if (languages.isEmpty()) {
+            throw new FindGuideException(ResponseCode.NOT_FOUND, "Could not find languages with names: " + args);
+        }
+        return languages;
     }
 }
