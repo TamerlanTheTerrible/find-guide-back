d958cbbf1370c7187f8abf8a25d68465c0b6b2e0
t.ismoilov <t.ismoilov@paymo.uz>
2023-07-08 09:36:48 +0500

    profiling

--- src/main/java/me/timur/findguideback/controller/ClientController.java
+++ src/main/java/me/timur/findguideback/controller/ClientController.java
@@ -2,9 +2,13 @@ package me.timur.findguideback.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.timur.findguideback.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
 
 /**
  * Created by Temurbek Ismoilov on 02/05/23.
@@ -15,9 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class ClientController {
     private final UserService userService;
+    private final Environment environment;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     @GetMapping(value = {"", "/"})
     public String index() {
+        System.out.println("Profile by environment: " + Arrays.toString(environment.getActiveProfiles()));
+        System.out.println("Profile by value: " + activeProfile);
         return "client";
     }
 }
