ac3fd79fc73c1b3a4ba625b7361cd6d7d5a9580b
t.ismoilov <t.ismoilov@paymo.uz>
2023-07-08 10:40:04 +0500

    fixes

--- src/main/java/me/timur/findguideback/controller/ClientController.java
+++ src/main/java/me/timur/findguideback/controller/ClientController.java
@@ -1,14 +1,11 @@
 package me.timur.findguideback.controller;
 
 import lombok.RequiredArgsConstructor;
-import me.timur.findguideback.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Arrays;
 
 /**
  * Created by Temurbek Ismoilov on 02/05/23.
@@ -18,16 +15,13 @@ import java.util.Arrays;
 @RequestMapping("/client")
 @RequiredArgsConstructor
 public class ClientController {
-    private final UserService userService;
-    private final Environment environment;
 
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
     @GetMapping(value = {"", "/"})
-    public String index() {
-        System.out.println("Profile by environment: " + Arrays.toString(environment.getActiveProfiles()));
-        System.out.println("Profile by value: " + activeProfile);
+    public String client(ModelMap model) {
+        model.addAttribute("profile", activeProfile);
         return "client";
     }
 }
